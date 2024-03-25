39c74a8c2efc8a9e47b2d792838995e7fafcfdd4
Emir Herrera <emir.herrera@gmail.com>
2024-03-25 13:46:41 -0600

    new: user: Hide quotation form until requested by user

--- app/js/form-processor.js
+++ app/js/form-processor.js
@@ -1,1 +1,4 @@
-console.log("A form processor");
+window.showQuotationForm = function showQuotationForm() {
+  document.getElementById("form-wrapper").classList.toggle("hidden");
+  document.getElementById("request-quote-invite").classList.toggle("hidden");
+};
