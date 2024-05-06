34cc17c70dfdf3a115641c0d0a44752e8edbaba3
Emir Herrera <emir.herrera@gmail.com>
2024-05-05 23:19:13 -0600

    new: dev: !wip New font weights added

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,21 @@ module.exports = {
     },
     fontFamily: {
       assistant: ["Assistant"],
+      sanspro: ['"Source Sans Pro"', "sans-serif"],
+      barlow: ['"Barlow Condensed"'],
+    },
+    //fontSize: {
+    // 18px   16px
+    // 24px   20px
+    // 26px   23px
+    // 35px   24px
+    // 85px   60px
+    //},
+    extend: {
+      fontSize: {
+        35: "35px",
+      },
     },
   },
-  presets: [],
   corePlugins: ["fontFamily", "fontSize", "fontWeight"],
 };
