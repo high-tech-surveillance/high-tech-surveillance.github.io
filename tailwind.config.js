23dac8fe911bc5c38908690e65e10f18b1afae0b
Emir Herrera <emir.herrera@gmail.com>
2024-07-19 21:01:32 -0600

    fix: user: Set proper font styles to Desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,18 @@ module.exports = {
     files: ["./app/**/*.{html,pug}"],
   },
   theme: {
+    fontFamily: {
+      assistant: ["Assistant"],
+      sanspro: ['"Source Sans Pro"', "sans-serif"],
+      barlow: ['"Barlow Condensed"'],
+    },
     colors: {
       black: "#000000",
+      white:'#FFF',
       red: {
         light: "#c11717",
         dark: "#a50d0d",
+        get DEFAULT(){ return this.light;}
       },
       blue: {
         DEFAULT: "#1153a7",
@@ -17,17 +24,12 @@ module.exports = {
     dropShadow: {
       md: "4px 4px 4px rgb(0 0 0 / 0.2)",
     },
-    fontFamily: {
-      assistant: ["Assistant"],
-      sanspro: ['"Source Sans Pro"', "sans-serif"],
-      barlow: ['"Barlow Condensed"'],
-    },
-    letterSpacing: {
-      wide: "0.26923em",
-    },
     extend: {
       lineHeight: {
-        "leading-tight": "1.2",
+        "tight": "1.1em",
+        "fit": "1.2em",
+        "snug": "1.4em",
+        "relaxed": "1.6em",
       },
       fontSize: {
         "85px": "85px", // Cover headline
@@ -36,7 +38,7 @@ module.exports = {
         "40px": "40px", // Each location higlight
         "36px": "36px", // About us, second title, image footer, phone number, Better yet!
         "35px": "35px", // Cover paragraph
-        "26px": "26px", // Cover second line, Remove survaillance second line
+        "26px": ["26px", "7px"], // Cover second line, Remove survaillance second line
         "24px": "24px", // Company name, cover button, Cover paragraph, call to action, Get quote button, quote paragraph, request quote, paragraph
         "18px": "18px", // Heading menu item, paragraph small, image legend, quote paragraph small, address
         "14px": "14px", // Input label, Form button, input combo
@@ -44,13 +46,7 @@ module.exports = {
       },
     },
   },
-  corePlugins: [
-    "fontFamily",
-    "fontSize",
-    "fontWeight",
-    "textDecoration",
-    "filter",
-    "dropShadow",
-    "letterSpacing",
-  ],
+  corePlugins: {
+    preflight: false,
+  }
 };
