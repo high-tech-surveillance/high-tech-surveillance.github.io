796c3a93fae2999c2b33fa69eb2f7b7b0099459e
Emir Herrera <emir.herrera@gmail.com>
2024-07-20 21:47:10 -0600

    chg: dev: New colors (blue) & font sizes
    * Fix a minor typo in the comments
    * Added new dark blue and darkest blue
    * Remove surveillance -> Remote surveillance
    * Move font sizes to the top of the config
    * Added font sizes for mobile first size

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,23 @@ module.exports = {
       sanspro: ['"Source Sans Pro"', "sans-serif"],
       barlow: ['"Barlow Condensed"'],
     },
+    fontSize: {
+      "85px": "85px", // Cover headline
+      "70px": "70px", // About us title
+      "60px": "60px",
+      "48px": "48px", // About us Services title, Remote surveillance title, Location Heading, bussiness card Heading, Request Heading, Company name
+      "40px": "40px", // Each location higlight
+      "36px": "36px", // About us, second title, image footer, phone number, Better yet!
+      "35px": "35px", // Cover paragraph
+      "26px": ["26px", {letterSpacing: "7px" }], // Cover second line, Remove survaillance second line
+      "25px": "25px",
+      "24px": "24px", // Company name, cover button, Cover paragraph, call to action, Get quote button, quote paragraph, request quote, paragraph
+      "23px": "23px",
+      "20px": "20px",
+      "18px": "18px", // Heading menu item, paragraph small, image legend, quote paragraph small, address
+      "14px": "14px", // Input label, Form button, input combo
+      "11px": "11px", // Form legend
+    },
     colors: {
       black: "#000000",
       white:'#FFF',
@@ -19,6 +36,8 @@ module.exports = {
       },
       blue: {
         DEFAULT: "#1153a7",
+        dark: "#051965",
+        darkest: "#13151E",
       },
     },
     dropShadow: {
@@ -30,19 +49,6 @@ module.exports = {
         "fit": "1.2em",
         "snug": "1.4em",
         "relaxed": "1.6em",
-      },
-      fontSize: {
-        "85px": "85px", // Cover headline
-        "70px": "70px", // About us title
-        "48px": "48px", // About us Services title, Remote surveillance title, Location Heading, bussiness card Heading, Request Heading, Company name
-        "40px": "40px", // Each location higlight
-        "36px": "36px", // About us, second title, image footer, phone number, Better yet!
-        "35px": "35px", // Cover paragraph
-        "26px": ["26px", {letterSpacing: "7px" }], // Cover second line, Remove survaillance second line
-        "24px": "24px", // Company name, cover button, Cover paragraph, call to action, Get quote button, quote paragraph, request quote, paragraph
-        "18px": "18px", // Heading menu item, paragraph small, image legend, quote paragraph small, address
-        "14px": "14px", // Input label, Form button, input combo
-        "11px": "11px", // Form legend
       },
     },
   },
